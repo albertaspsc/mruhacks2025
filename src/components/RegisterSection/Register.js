e9b1c050aae4443e1954529348293ff6adfb506a
methlab <methlab006@gmail.com>
2025-03-09 00:11:37 -0700

    Adding noise animation to register

--- src/components/RegisterSection/Register.js
+++ src/components/RegisterSection/Register.js
@@ -1,36 +1,110 @@
 "use client";
-import React from "react";
 import Image from "next/image";
-import styles from "./Register.module.css";
+import styles from "./Register.module.scss";
 import logo from "@/assets/logos/black-white-logo.png";
 
 const RegisterSection = () => {
   return (
-    <section id="register" className={styles.registerSection}>
-      <div className={styles.backgroundContainer}>
-        {/* Background image is set via CSS */}
-      </div>
-      <div className={styles.contentContainer}>
-        <div className={styles.logoContainer}>
-          <Image
-            src={logo}
-            alt="MRU Hackathon Logo"
-            priority
-            className={styles.logo}
+    <div id="register" className={styles.registerSection}>
+      <svg
+        style={{ position: "absolute", userSelect: "none" }}
+        width="200"
+        height="200"
+        viewBox="0 0 220 220"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <filter id="displacementFilter">
+          <feTurbulence
+            id="turbulence"
+            type="fractalNoise"
+            baseFrequency="0.5"
+            numOctaves="2"
+            seed="0"
+            result="fractalNoise"
           />
-        </div>
-        <div className={styles.buttonContainer}>
-          <a
-            href="https://forms.gle/YourRegistrationFormLink"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.registerButton}
-          >
-            Register Now
-          </a>
-        </div>
-      </div>
-    </section>
+          <feDisplacementMap
+            id="mapping"
+            in2="fractalNoise"
+            in="SourceGraphic"
+            scale="0"
+            xChannelSelector="R"
+            yChannelSelector="G"
+            result="noise"
+          />
+          <animate
+            xlinkHref="#turbulence"
+            attributeName="seed"
+            dur="0.8s"
+            keyTimes="0;0.5;1"
+            values="1;100;1000"
+            repeatCount="1"
+            begin="register.mouseenter"
+          ></animate>
+          <animate
+            xlinkHref="#mapping"
+            attributeName="scale"
+            dur="0.8s"
+            keyTimes="0;0.5;1"
+            values="0;40;0"
+            repeatCount="1"
+            begin="register.mouseenter"
+          ></animate>
+        </filter>
+        <filter id="lightDisplacementFilter">
+          <feTurbulence
+            id="turbulence"
+            type="fractalNoise"
+            baseFrequency="0.5"
+            numOctaves="2"
+            seed="0"
+            result="fractalNoise"
+          />
+          <feDisplacementMap
+            id="mapping"
+            in2="fractalNoise"
+            in="SourceGraphic"
+            scale="0"
+            xChannelSelector="R"
+            yChannelSelector="G"
+            result="noise"
+          />
+          <animate
+            xlinkHref="#turbulence"
+            attributeName="seed"
+            dur="0.8s"
+            keyTimes="0;0.5;1"
+            values="1;100;1000"
+            repeatCount="1"
+            begin="register.mouseenter"
+          ></animate>
+          <animate
+            xlinkHref="#mapping"
+            attributeName="scale"
+            dur="0.8s"
+            keyTimes="0;0.5;1"
+            values="0;20;0"
+            repeatCount="1"
+            begin="register.mouseenter"
+          ></animate>
+        </filter>
+      </svg>
+      <background id="background"></background>
+      <Image
+        src={logo}
+        alt="MRU Hackathon Logo"
+        priority
+        className={styles.logo}
+      />
+
+      <a
+        href="https://forms.gle/YourRegistrationFormLink"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.registerButton}
+      >
+        Register Now
+      </a>
+    </div>
   );
 };
 
