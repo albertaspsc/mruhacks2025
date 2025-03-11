d39586e81d61a9ebccaea18592a39514e55adfe6
methlab <methlab006@gmail.com>
2025-03-10 22:35:37 -0600

    Fixed links and spacing

--- src/components/Sponsor.js/Sponsor.jsx
+++ src/components/Sponsor.js/Sponsor.jsx
@@ -8,6 +8,14 @@ import library from "@/assets/sponsors/mrulibrary.webp";
 import arrowIcon from "@/assets/icons/arrow_forward_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
 
 export default function Sponsor() {
+  const sponsors = [
+    { img: acurve, href: "https://www.arcurve.com" },
+    {
+      img: institute,
+      href: "https://www.mtroyal.ca/ProgramsCourses/FacultiesSchoolsCentres/Business/Institutes/InstituteInnovationEntrepreneurship/index.htm",
+    },
+    { img: library, href: "https://library.mtroyal.ca" },
+  ];
   return (
     <>
       <div className={styles.container}>
@@ -16,17 +24,17 @@ export default function Sponsor() {
         <div>
           {" "}
           <div className={styles.sponsors}>
-            {[acurve, institute, library].map((x, i) => (
-              <a key={`sponsor-img-${i}`} href="https://example.com/">
-                <Image src={x} alt={`Sponsor ${i + 1} logo`} />
+            {sponsors.map(({ img, href }, i) => (
+              <a key={`sponsor-img-${i}`} href={href}>
+                <Image src={img} alt={`Sponsor ${i + 1} logo`} />
               </a>
             ))}
           </div>
         </div>
         <div className={styles["become-sponsor-container"]}>
-          <a href="https://example.com/">
+          <a href="mailto:mramz980@mtroyal.ca">
             <span>Become a sponsor</span>
-            <Image src={arrowIcon} />
+            <Image src={arrowIcon} alt="arrow left" />
           </a>
           <div className="crt">
             <Image src={crt2} alt="CRT tv mascot" />
