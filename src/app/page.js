31b3b7f63b35112edd03107243352f950842065d
Asia Ongjoco <aongj148@mtroyal.ca>
2025-03-14 08:01:58 -0600

    meet our team section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
+"use client";
+
+import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import RegisterSection from "../components/RegisterSection/Register";
 import Gallery from "../components/Gallery/Gallery";
 import Stats from "../components/Stats/Stats";
 import Sponsor from "../components/Sponsor/Sponsor";
 import Footer from "../components/Footer/Footer";
+import TeamCarousel from "../components/Carousel/TeamCarousel";
+import teamDataJson from "../data/team.json";
 
 export default function Home() {
+  const [teamData] = useState(teamDataJson);
+
   return (
     <>
       <Navbar />
@@ -20,6 +27,18 @@ export default function Home() {
 
         <section>
           <Gallery />
+        </section>
+
+        <section id="team" className="py-16 px-4 md:px-8">
+          <TeamCarousel
+            teamData={teamData}
+            options={{
+              loop: true,
+              align: "start",
+              dragFree: false,
+              containScroll: "trimSnaps",
+            }}
+          />
         </section>
 
         <section id="sponsors">
