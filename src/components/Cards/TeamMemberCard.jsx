131707b9ebe6f570d0bc4f419b7c63c404b423e2
Asia Ongjoco <aongj148@mtroyal.ca>
2025-03-21 08:33:44 -0600

    Team image optimization

--- src/components/Cards/TeamMemberCard.jsx
+++ src/components/Cards/TeamMemberCard.jsx
@@ -1,21 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 import styles from "./TeamMemberCard.module.css";
+import fallbackImage from "../../assets/mascots/crt2.png"; // Fallback image
 
 const TeamMemberCard = ({ member }) => {
+  const [imgSrc, setImgSrc] = useState(`/team/${member.pic.split("/").pop()}`);
+  const [imgError, setImgError] = useState(false);
+
+  const handleImageError = () => {
+    setImgError(true);
+    setImgSrc(fallbackImage.src); // Use the imported image's src property
+  };
+
   return (
     <div className={styles.memberCardContainer}>
       <div className={styles.memberCardWrapper}>
         <div className={styles.memberCardInner}>
           <div className={styles.imageContainer}>
-            <img
-              src={`/team/${member.pic.split("/").pop()}`}
+            <Image
+              src={imgSrc}
               alt={member.name}
               className={styles.memberImage}
-              onError={(e) => {
-                e.target.src = "../../assets/mascots/crt2.png"; // Fallback image
-              }}
+              onError={handleImageError}
+              width={150}
+              height={150}
+              style={{ objectFit: "cover" }}
+              unoptimized={imgError} // Skips optimization for fallback image
             />
           </div>
         </div>
