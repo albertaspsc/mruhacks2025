50c9ba3cc5c738dcbb7213a0fac2e4be6e49ca87
Asia Ongjoco <aongj148@mtroyal.ca>
2025-09-17 15:42:34 -0600

    Added Village Icecream logo and changed to TS

--- src/components/Sponsor/Sponsor.tsx
+++ src/components/Sponsor/Sponsor.tsx
@@ -7,13 +7,19 @@ import institute from "../../assets/sponsors/Institute.webp";
 import library from "../../assets/sponsors/mrulibrary.webp";
 import adx from "../../assets/sponsors/adx_logo.webp";
 import cnrl from "../../assets/sponsors/canadian_natural.webp";
+import village from "../../assets/sponsors/village_icecream.webp";
 import arrowIcon from "../../assets/icons/arrow_forward_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
 
+interface SponsorItem {
+  img: any;
+  href: string;
+}
+
 export default function Sponsor() {
-  const sponsorFormLink =
+  const sponsorFormLink: string =
     "https://docs.google.com/forms/d/e/1FAIpQLSdLJibHIC662j9-rTOG31qJ8iaTBgSk-Rk3LXAD0l9TL7MYpQ/viewform?usp=header";
 
-  const sponsors = [
+  const sponsors: SponsorItem[] = [
     { img: acurve, href: "https://www.arcurve.com" },
     {
       img: institute,
@@ -25,6 +31,7 @@ export default function Sponsor() {
       href: "https://www.abdiamond.ca/?gad_source=1&gad_campaignid=21486602802&gbraid=0AAAAAocBOJOMK1uXMlxXqUi7Ec0sjwsCv&gclid=CjwKCAjw4efDBhATEiwAaDBpbnF6Hno1EYFTfi94tso6NuHseNyx1TkGy1qJ4buIIP-2VS2SPyi6uxoCLN0QAvD_BwE",
     },
     { img: cnrl, href: "https://www.cnrl.com/" },
+    { img: village, href: "https://villageicecream.com/" },
   ];
 
   return (
@@ -33,7 +40,7 @@ export default function Sponsor() {
       <p>MRUHacks would be impossible without our fantastic sponsors.</p>
 
       <div className={styles.sponsors}>
-        {sponsors.map(({ img, href }, i) => (
+        {sponsors.map(({ img, href }, i: number) => (
           <a
             key={`sponsor-img-${i}`}
             href={href}
