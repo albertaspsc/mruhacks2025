88396951949a112d5ea71a43f40ec414553c4813
Sunny Parmar <113143064+aparm539@users.noreply.github.com>
2025-09-19 11:26:00 -0600

    Moving user/profile and user/dashboard pages to server components (#118)
    Co-authored-by: methlab <methlab006@gmail.com>

--- src/components/dashboards/checklist/Checklist.tsx
+++ src/components/dashboards/checklist/Checklist.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { MessageSquare, Trophy, AlertCircle, Loader2 } from "lucide-react";
