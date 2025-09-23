52d136e8a921219a3c0fd38af96b6fede1f57fff
Thomas Kapocsi <thomas@kapocsi.ca>
2025-09-23 09:15:46 -0600

    Fix merge error

--- src/app/user/dashboard/rsvp.tsx
+++ src/app/user/dashboard/rsvp.tsx
@@ -1,8 +1,6 @@
 "use server";
 
 import DashboardItem from "@/components/dashboards/common/DashboardItem";
-
-import DashboardItem from "@/components/dashboards/common/DashboardItem";
 import { createClient } from "@/utils/supabase/server";
 import RSVPButton, { ConfirmDecline } from "./confirm_button";
 import { RegistrationStatus } from "@/components/dashboards/common/StatusBanner";
@@ -37,7 +35,6 @@ export default async function Rsvp() {
   };
 
   if (status == "denied" || status == "declined" || !userCanRSVP) return null;
-
 
   return (
     <DashboardItem
